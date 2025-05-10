a2fbd076d65e1c81db2c766332bac12ea912d447
Sebastian Quiles <sequiles@mpf.gov.ar>
2025-05-10 18:45:09 -0300

    tests for DMY class

--- src/tests/test_init.py
+++ src/tests/test_init.py
@@ -5,10 +5,15 @@ from unittest import TestCase
 
 from pydantic import BaseModel
 
-from karpyncho.pydantic_extensions import DateSerializerMixin
+from karpyncho.pydantic_extensions import DateSerializerMixin, DateDMYSerializerMixin
 
 
 class MyDataClass(DateSerializerMixin, BaseModel):
+    str_field: str
+    date_field: date
+
+
+class MyDataDMYClass(DateDMYSerializerMixin, BaseModel):
     str_field: str
     date_field: date
 
@@ -27,8 +32,28 @@ class DateSerializerMixinTest(TestCase):
         self.assertEqual(obj.date_field, date(2019, 5, 23))
 
     def test_date_serializer_mixin_deserialize_value_error(self):
-        json_raw = '{"str_field": "hola", "date_field": "this isnot a date"}'
+        json_raw = '{"str_field": "hola", "date_field": "THIS IS NOT A DATE"}'
         my_dict = json.loads(json_raw)
 
         with self.assertRaises(ValueError):
             MyDataClass(**my_dict)
+
+class DateDMYSerializerMixinTest(TestCase):
+
+    def test_date_dmy_serializer_mixin_serialize(self):
+        data = MyDataDMYClass(str_field="Hola", date_field=date(2023, 1, 13))
+        self.assertEqual(data.model_dump(), {"str_field": "Hola", "date_field": "13/01/2023"})
+
+    def test_date_dmy_serializer_mixin_deserialize(self):
+        json_raw = '{"str_field": "hola", "date_field": "23/05/2019"}'
+        my_dict = json.loads(json_raw)
+        obj = MyDataDMYClass(**my_dict)
+
+        self.assertEqual(obj.date_field, date(2019, 5, 23))
+
+    def test_date_dmy_serializer_mixin_deserialize_value_error(self):
+        json_raw = '{"str_field": "hola", "date_field": "THIS IS NOT A DATE"}'
+        my_dict = json.loads(json_raw)
+
+        with self.assertRaises(ValueError):
+            MyDataDMYClass(**my_dict)
